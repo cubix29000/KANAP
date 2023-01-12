696e5581bf1609768c770ff4320202ef290d7060
Kériel Loïc <108335272+cubix29000@users.noreply.github.com>
2023-01-12 14:25:26 +0100

    Delete confirmation.js

--- front/js/confirmation.js
+++
@@ -1,8 +0,0 @@
-// Récupération de l'orderId du produit via l' URL
-const orderId = new URL(window.location).searchParams.get("order_id");
-
-// affichage du numéro de commande
-function displayOrderId(numId) {
-    document.getElementById("orderId").innerText = `${numId}`;
-}
-displayOrderId(orderId);
